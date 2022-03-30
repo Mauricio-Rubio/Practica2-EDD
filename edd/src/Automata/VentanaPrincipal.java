a892bb7b9b22dd2e96f69b27c9e548abddc00e96
Mauricio Rubio Haro <mauricio.rubio@ciencias.unam.mx>
2022-03-29 18:35:58 -0600

    Added colors in Imagen class

--- edd/src/Automata/VentanaPrincipal.java
+++ edd/src/Automata/VentanaPrincipal.java
@@ -58,7 +58,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
          * AutomataCelular a = new juego();
          */
        //AutomataCelular a = new JuegoVida();
-       AutomataCelular a = new Mondrian();
+       AutomataCelular a = new Space();
        //AutomataCelular a=new juego();
        vp = new VentanaPrincipal(a);
     }
